62441c4510d7a515020e80a7afee463206d05ebf
whq <whq11523@163.com>
2022-09-01 15:13:05 +0800

    1

---
+++ src/test/java/com/sifan/sifanos/SifanOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sifan.sifanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SifanOsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
